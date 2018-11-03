7e3d0024f68a4d932c45b04280c978df7e003f90
ForthEorlingas <44584319+ForthEorlingas@users.noreply.github.com>
2018-11-03 11:31:47 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Mail
 
             using (SmtpClient client = new SmtpClient())
             {
-                //podajemy dane dostępowe
+
                 var credential = new NetworkCredential
                 {
                     UserName = "user",
@@ -19,26 +19,24 @@ namespace Mail
                 };
                 client.Credentials = credential;
 
-                //host oraz port poczty,
-                //dostawca udostępnia nam te dane
                 client.Host = "smtp.gmail.com";
                 client.Port = 587;
                 client.EnableSsl = true;
 
-                //tworzymy wiadomość
+
                 var message = new MailMessage();
 
-                //dodajemy odbiorców
+               
                 message.To.Add(new MailAddress("email_1@wp.pl"));
-                //podajemy adres nadawcy
+               
                 message.From = new MailAddress("email_2@gmail.com");
-                //Tytuł wiadomości
+              
                 message.Subject = "Raport";
                 message.Body = "We have sent logs into yor email!";
-                //Możemy uzyć znaczników html wewnątrz ciała wiadomości (parametr Body), w tym celu ustawiamy parametr na true
+               
                 message.IsBodyHtml = true;
 
-                //Opcjonalnie możemy również dodać załącznik
+
                 Attachment a = new Attachment("D:\\screen.png", System.Net.Mime.MediaTypeNames.Image.Jpeg);
                 message.Attachments.Add(a);
                 message.Attachments.Add(new Attachment("D:\\xd.txt"));
